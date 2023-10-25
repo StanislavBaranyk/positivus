d4f902fe1a96a32180779641d3a7f2d12a9b18bc
StanislavBaranyk <stasbaranyk@gmail.com>
2023-10-25 18:23:38 +0300

    changes php and js

--- index.php
+++ index.php
@@ -371,7 +371,7 @@
                         'class' => 'gray',
                         'headerOne' => 'Search engine',
                         'headerTwo' => 'optimization',
-                        'picture' => '<img src="images/services-cards-picture/cards-picture1.png">',
+                        'picture' => 'cards-picture1.png',
                         'btn' => '<svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" viewBox="0 0 41 41" fill="none">
                                     <circle cx="20.5" cy="20.5" r="20.5" fill="black"></circle>
                                     <path d="M11.2501 24.7009C10.5326 25.1151 10.2868 26.0325 10.701 26.75C11.1152 27.4674 12.0326 27.7132 12.7501 27.299L11.2501 24.7009ZM30.7695 16.3882C30.9839 15.588 30.509 14.7655 29.7088 14.5511L16.6688 11.057C15.8686 10.8426 15.0461 11.3175 14.8317 12.1177C14.6173 12.9179 15.0921 13.7404 15.8923 13.9548L27.4835 17.0606L24.3776 28.6517C24.1632 29.4519 24.6381 30.2744 25.4383 30.4889C26.2385 30.7033 27.061 30.2284 27.2754 29.4282L30.7695 16.3882ZM12.7501 27.299L30.0706 17.299L28.5706 14.7009L11.2501 24.7009L12.7501 27.299Z" fill="#B9FF66"></path>
@@ -381,52 +381,47 @@
                         'class' => 'green',
                         'headerOne' => 'Pay-per-click',
                         'headerTwo' => 'advertising',
-                        'picture' => '<img src="images/services-cards-picture/cards-picture2.png">',
+                        'picture' => 'cards-picture2.png',
                         'btn' => '<svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" viewBox="0 0 41 41" fill="none">
                                     <circle cx="20.5" cy="20.5" r="20.5" fill="black"></circle>
                                     <path d="M11.2501 24.7009C10.5326 25.1151 10.2868 26.0325 10.701 26.75C11.1152 27.4674 12.0326 27.7132 12.7501 27.299L11.2501 24.7009ZM30.7695 16.3882C30.9839 15.588 30.509 14.7655 29.7088 14.5511L16.6688 11.057C15.8686 10.8426 15.0461 11.3175 14.8317 12.1177C14.6173 12.9179 15.0921 13.7404 15.8923 13.9548L27.4835 17.0606L24.3776 28.6517C24.1632 29.4519 24.6381 30.2744 25.4383 30.4889C26.2385 30.7033 27.061 30.2284 27.2754 29.4282L30.7695 16.3882ZM12.7501 27.299L30.0706 17.299L28.5706 14.7009L11.2501 24.7009L12.7501 27.299Z" fill="#B9FF66"></path>
                                 </svg>',
-
                     ],
                     [
                         'class' => 'black',
                         'headerOne' => 'Social Media',
                         'headerTwo' => 'Marketing',
-                        'picture' => '<img src="images/services-cards-picture/cards-picture3.png">',
+                        'picture' => 'cards-picture3.png',
                         'btn' => '<svg xmlns="http://www.w3.org/2000/svg" width="41" height="42" viewBox="0 0 41 42" fill="none">
                                     <circle cx="20.5" cy="20.9707" r="20.5" fill="white"></circle>
                                     <path d="M11.2501 25.1716C10.5326 25.5858 10.2868 26.5032 10.701 27.2207C11.1152 27.9381 12.0326 28.1839 12.7501 27.7697L11.2501 25.1716ZM30.7695 16.8589C30.9839 16.0587 30.509 15.2362 29.7088 15.0218L16.6688 11.5277C15.8686 11.3133 15.0461 11.7882 14.8317 12.5884C14.6173 13.3886 15.0921 14.2111 15.8923 14.4255L27.4835 17.5313L24.3776 29.1224C24.1632 29.9226 24.6381 30.7451 25.4383 30.9596C26.2385 31.174 27.061 30.6991 27.2754 29.8989L30.7695 16.8589ZM12.7501 27.7697L30.0706 17.7697L28.5706 15.1716L11.2501 25.1716L12.7501 27.7697Z" fill="black"></path>
                                 </svg>',
-
-
                     ],
                     [
                         'class' => 'gray',
                         'headerOne' => 'Email',
                         'headerTwo' => 'Marketing',
-                        'picture' => '<img src="images/services-cards-picture/cards-picture4.png">',
+                        'picture' => 'cards-picture4.png',
                         'btn' => '<svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" viewBox="0 0 41 41" fill="none">
                                     <circle cx="20.5" cy="20.5" r="20.5" fill="black"></circle>
                                     <path d="M11.2501 24.7009C10.5326 25.1151 10.2868 26.0325 10.701 26.75C11.1152 27.4674 12.0326 27.7132 12.7501 27.299L11.2501 24.7009ZM30.7695 16.3882C30.9839 15.588 30.509 14.7655 29.7088 14.5511L16.6688 11.057C15.8686 10.8426 15.0461 11.3175 14.8317 12.1177C14.6173 12.9179 15.0921 13.7404 15.8923 13.9548L27.4835 17.0606L24.3776 28.6517C24.1632 29.4519 24.6381 30.2744 25.4383 30.4889C26.2385 30.7033 27.061 30.2284 27.2754 29.4282L30.7695 16.3882ZM12.7501 27.299L30.0706 17.299L28.5706 14.7009L11.2501 24.7009L12.7501 27.299Z" fill="#B9FF66"></path>
                                 </svg>',
-
                     ],
                     [
                         'class' => 'green',
                         'headerOne' => 'Content',
                         'headerTwo' => 'Creation',
-                        'picture' => '<img src="images/services-cards-picture/cards-picture5.png">',
+                        'picture' => 'cards-picture5.png',
                         'btn' => '<svg xmlns="http://www.w3.org/2000/svg" width="41" height="42" viewBox="0 0 41 42" fill="none">
                                     <circle cx="20.5" cy="20.9706" r="20.5" fill="black"></circle>
                                     <path d="M11.2501 25.1715C10.5326 25.5857 10.2868 26.5031 10.701 27.2205C11.1152 27.938 12.0326 28.1838 12.7501 27.7696L11.2501 25.1715ZM30.7695 16.8588C30.9839 16.0586 30.509 15.2361 29.7088 15.0217L16.6688 11.5276C15.8686 11.3132 15.0461 11.7881 14.8317 12.5883C14.6173 13.3885 15.0921 14.211 15.8923 14.4254L27.4835 17.5312L24.3776 29.1223C24.1632 29.9225 24.6381 30.745 25.4383 30.9594C26.2385 31.1738 27.061 30.699 27.2754 29.8988L30.7695 16.8588ZM12.7501 27.7696L30.0706 17.7696L28.5706 15.1715L11.2501 25.1715L12.7501 27.7696Z" fill="#B9FF66"></path>
                                 </svg>',
-
                     ],
                     [
                         'class' => 'black',
                         'headerOne' => 'Analytics and',
                         'headerTwo' => 'Tracking',
-                        'picture' => '<img src="images/services-cards-picture/cards-picture6.png">',
+                        'picture' => 'cards-picture6.png',
                         'btn' => '<svg xmlns="http://www.w3.org/2000/svg" width="41" height="42" viewBox="0 0 41 42" fill="none">
                                     <circle cx="20.5" cy="20.9707" r="20.5" fill="white"></circle>
                                     <path d="M11.2501 25.1716C10.5326 25.5858 10.2868 26.5032 10.701 27.2207C11.1152 27.9381 12.0326 28.1839 12.7501 27.7697L11.2501 25.1716ZM30.7695 16.8589C30.9839 16.0587 30.509 15.2362 29.7088 15.0218L16.6688 11.5277C15.8686 11.3133 15.0461 11.7882 14.8317 12.5884C14.6173 13.3886 15.0921 14.2111 15.8923 14.4255L27.4835 17.5313L24.3776 29.1224C24.1632 29.9226 24.6381 30.7451 25.4383 30.9596C26.2385 31.174 27.061 30.6991 27.2754 29.8989L30.7695 16.8589ZM12.7501 27.7697L30.0706 17.7697L28.5706 15.1716L11.2501 25.1716L12.7501 27.7697Z" fill="black"></path>
@@ -436,7 +431,6 @@
 
 
                 foreach ($cardsItems as $cardsItem) {
-
                     echo '              
                 <div class="cards-item ' . $cardsItem['class'] . '">
                     <div class="card-box">
@@ -456,14 +450,12 @@
                                 <p class="section-paragraph">Learn more</p>
                             </div>
                             <div class="card-picture">
-                                ' . $cardsItem['picture'] . '
+                                <img src="images/services-cards-picture/' . $cardsItem['picture'] . '">
                             </div>
                         </div>
                     </div>
                 </div>';
                 }
-
-
                 ?>
 
             </div>
@@ -641,7 +633,6 @@
                             <code>.accordion-body</code>, though the transition does limit overflow.
                         ";
 
-
                 $accordionItems = [
                     [
                         'number' => '01',
@@ -691,8 +682,6 @@
                     <div class="accordion-body">' . $accordionItem['accordionText'] . '</div>
                 </div>
             </div>';
-
-
                 }
 
 
@@ -721,7 +710,7 @@
                 $teamCardsContent = [
 
                     [
-                        'picture' => '<img src="images/team-picture/team-card-picture1.png">',
+                        'picture' => 'team-card-picture1.png',
                         'name' => 'John Smith',
                         'specialty' => 'CEO and Founder',
                         'description' => '10+ years of experience in digital marketing. Expertise in SEO,
@@ -729,21 +718,21 @@
                             and content strategy',
                     ],
                     [
-                        'picture' => '<img src="images/team-picture/team-card-picture2.png">',
+                        'picture' => 'team-card-picture2.png',
                         'name' => 'Jane Doe',
                         'specialty' => 'Director of Operations',
                         'description' => '7+ years of experience in project management and team leadership.
                             Strong organizational and communication skills',
                     ],
                     [
-                        'picture' => '<img src="images/team-picture/team-card-picture3.png">',
+                        'picture' => 'team-card-picture3.png',
                         'name' => 'Michael Brown',
                         'specialty' => 'Senior SEO Specialist',
                         'description' => '5+ years of experience in SEO and content creation. Proficient in
                             keyword research and on-page optimization',
                     ],
                     [
-                        'picture' => '<img src="images/team-picture/team-card-picture4.png">',
+                        'picture' => 'team-card-picture4.png',
                         'name' => 'Emily Johnson',
                         'specialty' => 'PPC Manager',
                         'description' => '3+ years of experience in paid search advertising. Skilled in
@@ -752,7 +741,7 @@
                     ],
                     [
                         'class' => 'last-remove',
-                        'picture' => '<img src="images/team-picture/team-card-picture5.png">',
+                        'picture' => 'team-card-picture5.png',
                         'name' => 'Brian Williams',
                         'specialty' => 'Social Media Specialist',
                         'description' => '4+ years of experience in social media marketing. Proficient in
@@ -760,7 +749,7 @@
                     ],
                     [
                         'class' => 'last-remove',
-                        'picture' => '<img src="images/team-picture/team-card-picture6.png">',
+                        'picture' => 'team-card-picture6.png',
                         'name' => 'Sarah Kim',
                         'specialty' => 'Content Creator',
                         'description' => '2+ years of experience in writing and editing
@@ -775,7 +764,7 @@
                 <div class="section-item ' . $teamCardContent['class'] . '">
                     <div class="team-card">
                         <div class="team-picture">
-                        ' . $teamCardContent['picture'] . '
+                        <img src="images/team-picture/' . $teamCardContent['picture'] . '">
                             <div class="team-name">
                                 <h5>' . $teamCardContent['name'] . '</h5>
                                 <p class="section-paragraph">' . $teamCardContent['specialty'] . '</p>
@@ -824,6 +813,17 @@
                                     Positivus
                                     to any company looking to grow their online presence."',
 
+                            'name' => 'John Smith',
+                            'specialty' => 'Marketing Director at XYZ Corp',
+                        ],
+                        [
+                            'description' => '"We have been working with Positivus for the past year and
+                                    have
+                                    seen a significant increase
+                                    in website traffic and leads as a result of their efforts. The team is professional,
+                                    responsive, and truly cares about the success of our business. We highly recommend
+                                    Positivus
+                                    to any company looking to grow their online presence."',
 
                             'name' => 'John Smith',
                             'specialty' => 'Marketing Director at XYZ Corp',
@@ -837,7 +837,6 @@
                                     Positivus
                                     to any company looking to grow their online presence."',
 
-
                             'name' => 'John Smith',
                             'specialty' => 'Marketing Director at XYZ Corp',
                         ],
@@ -850,7 +849,6 @@
                                     Positivus
                                     to any company looking to grow their online presence."',
 
-
                             'name' => 'John Smith',
                             'specialty' => 'Marketing Director at XYZ Corp',
                         ],
@@ -862,20 +860,6 @@
                                     responsive, and truly cares about the success of our business. We highly recommend
                                     Positivus
                                     to any company looking to grow their online presence."',
-
-
-                            'name' => 'John Smith',
-                            'specialty' => 'Marketing Director at XYZ Corp',
-                        ],
-                        [
-                            'description' => '"We have been working with Positivus for the past year and
-                                    have
-                                    seen a significant increase
-                                    in website traffic and leads as a result of their efforts. The team is professional,
-                                    responsive, and truly cares about the success of our business. We highly recommend
-                                    Positivus
-                                    to any company looking to grow their online presence."',
-
 
                             'name' => 'John Smith',
                             'specialty' => 'Marketing Director at XYZ Corp',
@@ -887,17 +871,16 @@
                     <div class="testimonials-slider-col">
                         <div class="testimonials-slider-content">
                             <div class="testimonials-slider-text">
-                                <p class="section-paragraph">'. $testimonialsSliderItem['description'] .'</p>
+                                <p class="section-paragraph">' . $testimonialsSliderItem['description'] . '</p>
 
                                 <img src="images/testimonials/polugon.png">
                             </div>
                             <div class="testimonials-slider-person">
-                                <h4>'. $testimonialsSliderItem['name'] .'</h4>
-                                <p class="section-paragraph">'. $testimonialsSliderItem['specialty'] .'</p>
+                                <h4>' . $testimonialsSliderItem['name'] . '</h4>
+                                <p class="section-paragraph">' . $testimonialsSliderItem['specialty'] . '</p>
                             </div>
                         </div>
                     </div>';
-
                     }
 
 
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var caseSlider = tns({
     controls: false,
     responsive: {
 
-        0 :{
+        0: {
             items: 1,
             gutter: 20,
         },
@@ -26,11 +26,11 @@ var testimonialsSlider = tns({
   <g opacity="0.3">
     <path d="M22 13.5C22.8284 13.5 23.5 12.8284 23.5 12C23.5 11.1716 22.8284 10.5 22 10.5L22 13.5ZM0.939341 10.9393C0.353554 11.5251 0.353554 12.4749 0.93934 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92893 13.1924 1.97918 12.6066 1.3934C12.0208 0.807611 11.0711 0.807611 10.4853 1.3934L0.939341 10.9393ZM22 10.5L2 10.5L2 13.5L22 13.5L22 10.5Z" fill="white"/>
   </g>
-</svg>`,`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+</svg>`, `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
   <path d="M2 10.5C1.17157 10.5 0.5 11.1716 0.5 12C0.5 12.8284 1.17157 13.5 2 13.5L2 10.5ZM23.0607 13.0607C23.6464 12.4749 23.6464 11.5251 23.0607 10.9393L13.5147 1.3934C12.9289 0.807613 11.9792 0.807613 11.3934 1.3934C10.8076 1.97919 10.8076 2.92893 11.3934 3.51472L19.8787 12L11.3934 20.4853C10.8076 21.0711 10.8076 22.0208 11.3934 22.6066C11.9792 23.1924 12.9289 23.1924 13.5147 22.6066L23.0607 13.0607ZM2 13.5L22 13.5L22 10.5L2 10.5L2 13.5Z" fill="white"/>
 </svg>`],
 
-    responsive:  {
+    responsive: {
         0: {
             items: 1,
             gutter: 50,
@@ -74,123 +74,6 @@ smallNavActivated.addEventListener('click', () => {
 })
 
 
-let accordionItemOne = document.getElementById('accordion-itemOne');
-let accordionItemTwo = document.getElementById('accordion-itemTwo');
-let accordionItemThere = document.getElementById('accordion-itemThere');
-let accordionItemFour = document.getElementById('accordion-itemFour');
-let accordionItemFive = document.getElementById('accordion-itemFive');
-let accordionItemSix = document.getElementById('accordion-itemSix');
-
-let accordionBtnOne = document.getElementById('btn-itemOne');
-let accordionBtnTwo = document.getElementById('btn-itemTwo');
-let accordionBtnThere = document.getElementById('btn-itemThere');
-let accordionBtnFour = document.getElementById('btn-itemFour');
-let accordionBtnFive = document.getElementById('btn-itemFive');
-let accordionBtnSix = document.getElementById('btn-itemSix');
-
-accordionBtnOne.addEventListener('click', () => {
-    accordionItemTwo.classList.remove('green-accordion');
-    accordionItemThere.classList.remove('green-accordion');
-    accordionItemFour.classList.remove('green-accordion');
-    accordionItemFive.classList.remove('green-accordion');
-    accordionItemSix.classList.remove('green-accordion');
-
-
-
-    if (!accordionItemOne.classList.contains('green-accordion')) {
-        accordionItemOne.classList.add('green-accordion')
-    }
-    else if (accordionItemOne.classList.contains('green-accordion')) {
-        accordionItemOne.classList.remove('green-accordion');
-    }
-})
-
-accordionBtnTwo.addEventListener('click', () => {
-    accordionItemOne.classList.remove('green-accordion');
-    accordionItemThere.classList.remove('green-accordion');
-    accordionItemFour.classList.remove('green-accordion');
-    accordionItemFive.classList.remove('green-accordion');
-    accordionItemSix.classList.remove('green-accordion');
-
-
-
-    if (!accordionItemTwo.classList.contains('green-accordion')) {
-        accordionItemTwo.classList.add('green-accordion')
-    }
-    else if (accordionItemTwo.classList.contains('green-accordion')) {
-        accordionItemTwo.classList.remove('green-accordion');
-    }
-})
-
-accordionBtnThere.addEventListener('click', () => {
-    accordionItemOne.classList.remove('green-accordion');
-    accordionItemTwo.classList.remove('green-accordion');
-    accordionItemFour.classList.remove('green-accordion');
-    accordionItemFive.classList.remove('green-accordion');
-    accordionItemSix.classList.remove('green-accordion');
-
-
-
-    if (!accordionItemThere.classList.contains('green-accordion')) {
-        accordionItemThere.classList.add('green-accordion')
-    }
-    else if (accordionItemThere.classList.contains('green-accordion')) {
-        accordionItemThere.classList.remove('green-accordion');
-    }
-})
-
-accordionBtnFour.addEventListener('click', () => {
-    accordionItemOne.classList.remove('green-accordion');
-    accordionItemTwo.classList.remove('green-accordion');
-    accordionItemThere.classList.remove('green-accordion');
-    accordionItemFive.classList.remove('green-accordion');
-    accordionItemSix.classList.remove('green-accordion');
-
-
-
-    if (!accordionItemFour.classList.contains('green-accordion')) {
-        accordionItemFour.classList.add('green-accordion')
-    }
-    else if (accordionItemFour.classList.contains('green-accordion')) {
-        accordionItemFour.classList.remove('green-accordion');
-    }
-})
-
-accordionBtnFive.addEventListener('click', () => {
-    accordionItemOne.classList.remove('green-accordion');
-    accordionItemTwo.classList.remove('green-accordion');
-    accordionItemThere.classList.remove('green-accordion');
-    accordionItemFour.classList.remove('green-accordion');
-    accordionItemSix.classList.remove('green-accordion');
-
-
-
-    if (!accordionItemFive.classList.contains('green-accordion')) {
-        accordionItemFive.classList.add('green-accordion')
-    }
-    else if (accordionItemFive.classList.contains('green-accordion')) {
-        accordionItemFive.classList.remove('green-accordion');
-    }
-})
-
-accordionBtnSix.addEventListener('click', () => {
-    accordionItemOne.classList.remove('green-accordion');
-    accordionItemTwo.classList.remove('green-accordion');
-    accordionItemThere.classList.remove('green-accordion');
-    accordionItemFour.classList.remove('green-accordion');
-    accordionItemFive.classList.remove('green-accordion');
-
-
-
-    if (!accordionItemSix.classList.contains('green-accordion')) {
-        accordionItemSix.classList.add('green-accordion')
-    }
-    else if (accordionItemSix.classList.contains('green-accordion')) {
-        accordionItemSix.classList.remove('green-accordion');
-    }
-})
-
-
 let contactContactBtnOne = document.getElementById('say-hi');
 let contactContactBtnTwo = document.getElementById('get-quote');
 
@@ -215,11 +98,10 @@ contactContactBtnTwo.addEventListener('click', () => {
 function checkWindowWidth() {
     const windowWidth = window.innerWidth;
     const breakpoint = 992; // Ширина, при якій акордеон буде відкритим
-    const btnItemOne = document.getElementById('btn-itemOne');
+    const btnItem0 = document.getElementById('btn-item0');
 
     if (windowWidth >= breakpoint) {
-        // Виконуємо клік на кнопці акордеону, якщо ширина вікна більша або рівна breakpoint
-        btnItemOne.click();
+        btnItem0.click();
     }
 }
 
@@ -228,5 +110,26 @@ window.addEventListener('load', checkWindowWidth);
 
 // Викликаємо функцію при зміні розміру вікна
 window.addEventListener('resize', checkWindowWidth);
+
+
+const accordionCollapses = document.querySelectorAll('.accordion-collapse');
+
+accordionCollapses.forEach(accordionCollapse => {
+    accordionCollapse.addEventListener('show.bs.collapse', event => {
+        event.target.closest('.accordion-item').classList.add('green-accordion')
+    })
+
+    accordionCollapse.addEventListener('hide.bs.collapse', event => {
+        event.target.closest('.accordion-item').classList.remove('green-accordion')
+    })
+})
+
+
+
+
+
+
+
+
 
 
